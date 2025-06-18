f932a07de468167d656c46d9f4c6bbab285c28b6
Safayet <hossainsafayet187@gmail.com>
2025-06-18 21:17:24 +0600

    variable

---
+++ var.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Declare and initialize variables
+	var name string = "Alice"
+	var age int = 30
+	var height float64 = 5.7
+	var isStudent bool = false
+
+	// Short variable declaration (type inferred)
+	city := "New York"
+
+	// Print variables
+	fmt.Println("Name:", name)
+	fmt.Println("Age:", age)
+	fmt.Println("Height:", height)
+	fmt.Println("Is student:", isStudent)
+	fmt.Println("City:", city)
+}
