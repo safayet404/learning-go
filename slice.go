1337c7ef9914f3e8143b8617b042dd4f377df654
safayet404 <hossainsafayet187@gmail.com>
2026-02-25 00:55:56 +0600

    make slice

--- slice.go
+++ slice.go
@@ -3,9 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	s := []int{1, 2, 5}
+	// s := []int{1, 2, 5}
+
+	// fmt.Println(s)
+	// fmt.Println(len(s))
+	// fmt.Println(cap(s))
+
+	s := make([]int, 3, 5)
+
+	s[0] = 10
+	s[2] = 50
 
 	fmt.Println(s)
-	fmt.Println(len(s))
-	fmt.Println(cap(s))
+
 }
