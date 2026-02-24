e4cf73d9f86c4fc877db2f512f8778e27b3ae3df
safayet404 <hossainsafayet187@gmail.com>
2026-02-24 23:31:13 +0600

    pointer

--- pointer.go
+++ pointer.go
@@ -2,15 +2,22 @@ package main
 
 import "fmt"
 
+func print(numbers *[3]int) {
+	fmt.Println(numbers)
+}
+
 func main() {
 
-	x := 10
+	// x := 10
 
-	p := &x
+	// p := &x
 
-	*p = 20
+	// *p = 20
 
-	fmt.Println("Address", p)
-	fmt.Println("Value at address", *p)
+	// fmt.Println("Address", p)
+	// fmt.Println("Value at address", *p)
+
+	arr := [3]int{1, 2, 3}
+	print(&arr)
 
 }
